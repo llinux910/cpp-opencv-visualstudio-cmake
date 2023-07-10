ce9c81dbe7407464039d9db6bae28e56950462d1
linux910 <llinux910@gmail.com>
2023-07-10 13:41:55 +0900

    test

--- build.bat
+++ build.bat
@@ -1,19 +1,14 @@
-@echo off
 
 
 @echo off
 cd opencv_zips
 
-REM 분할 압축된 opencv 압축해제 대상 디렉토리 경로 설정
 set target_dir="../opencv"
 
-REM 7-Zip 실행 파일 경로 설정 (7z.exe 파일이 있는 경로로 설정해야 함)
 set zip_executable="C:\Program Files\7-Zip\7z.exe"
 
-REM 분할 압축 해제
 %zip_executable% x -o"%target_dir%" opencv.zip
 
-REM 압축 해제 완료 메시지 출력
 echo opencv 압축 해제가 완료되었습니다.
 
 
